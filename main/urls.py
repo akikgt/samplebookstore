7b0c262aefc9fc1c76086a28bce325494bdb59f4
akiair <none>
2019-12-04 17:48:30 +0900

    Add test and address pages

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,26 @@ from django.contrib.auth import views as auth_views
 from main import views, models, forms
 
 urlpatterns = [
+     path(
+        "address/",
+        views.AddressListView.as_view(),
+        name="address_list",
+    ),
+    path(
+       "address/create/",
+        views.AddressCreateView.as_view(),
+        name="address_create",
+    ),
+    path(
+       "address/<int:pk>/",
+       views.AddressUpdateView.as_view(),
+       name="address_update",
+    ),
+    path(
+       "address/<int:pk>/delete/",
+       views.AddressDeleteView.as_view(),
+       name="address_delete",
+    ),
     path(
         "login/",
         auth_views.LoginView.as_view(
